4e9fe3afd64395aa699ffb24e5a69fba741415ca
bKedds <benkeddie@gmail.com>
2020-07-21 11:56:43 -0700

    Updates

---
+++ precache-manifest.7b92b7a079eafe82b168413721aafad9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31c09aa7103b591e15a22f630f4738f8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0b72e73370be65fabf88",
+    "url": "/monsters-rolodex/static/css/main.4a1dc163.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b72e73370be65fabf88",
+    "url": "/monsters-rolodex/static/js/main.5c72c131.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
